a3796fc17ec1c53cc099daa4ca8cf0495d83e283
kapitainsky <dariuszb@me.com>
2019-09-26 11:57:07 +0100

    release_macOS.sh update
    change
    
    release_macOS.sh update

--- scripts/release_macOS.sh
+++ scripts/release_macOS.sh
@@ -2,7 +2,10 @@
 
 set -e
 
-QTDIR=/usr/local/Cellar/Qt/5.13.1
+export LDFLAGS="-L/usr/local/opt/qt/lib"
+export CPPFLAGS="-I/usr/local/opt/qt/include"
+
+QTDIR=/usr/local/opt/qt
 
 ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/..
 VERSION=`cat $ROOT/VERSION`-`git rev-parse --short HEAD`
@@ -14,10 +17,15 @@ rm -rf "$BUILD"
 mkdir -p "$BUILD"
 cd "$BUILD"
 cmake .. -DCMAKE_PREFIX_PATH="$QTDIR" -DCMAKE_BUILD_TYPE=Release
-make -j2
-cd ..
+make -j4
+cd build
+"$QTDIR"/bin/macdeployqt rclone-browser.app -executable="rclone-browser.app/Contents/MacOS/rclone-browser" -qmldir=../src/
+cd ../..
 
-rm -rf "$TARGET"
+
+mkdir -p release
+cd release
+rm -rf "$TARGET"*
 mkdir "$TARGET"
 cp "$ROOT"/README.md "$TARGET"/Readme.txt
 cp "$ROOT"/CHANGELOG.md "$TARGET"/Changelog.txt
@@ -28,42 +36,11 @@ mv "$APP"/Contents/MacOS/rclone-browser "$APP"/Contents/MacOS/"Rclone Browser"
 sed -i .bak 's/rclone-browser/Rclone Browser/g' "$APP"/Contents/Info.plist
 rm "$APP"/Contents/*.bak
 
-FRAMEWORKS=("Core" "Gui" "Widgets" "PrintSupport" "MacExtras")
-
-mkdir "$APP"/Contents/Frameworks
-for FX in "${FRAMEWORKS[@]}"
-do
-  cp -R "$QTDIR"/lib/Qt${FX}.framework "$APP"/Contents/Frameworks/
-  FXPATH="$APP"/Contents/Frameworks/Qt${FX}.framework
-  install_name_tool -id @executable_path/../Frameworks/Qt${FX}.framework/Versions/5/Qt${FX} "${FXPATH}"/Versions/5/Qt${FX}
-  rm -rf "$FXPATH"/Headers
-  rm -rf "$FXPATH"/*.prl
-  rm -rf "$FXPATH"/*_debug
-  rm -rf "$FXPATH"/Versions/5/Headers
-  rm -rf "$FXPATH"/Versions/5/*_debug
-done
-
-mkdir -p "$APP"/Contents/Plugins/platforms
-cp "$QTDIR"/plugins/platforms/libqcocoa.dylib "$APP"/Contents/Plugins/platforms
-
-change()
-{
-  for x in $2
-  do
-    install_name_tool -change @rpath/Qt$x.framework/Versions/5/Qt$x @executable_path/../Frameworks/Qt$x.framework/Versions/5/Qt$x "$1"
-  done
-}
-
-change "$APP"/Contents/MacOS/"Rclone Browser" "Core Gui Widgets MacExtras"
-change "$APP"/Contents/Frameworks/QtGui.framework/QtGui "Core"
-change "$APP"/Contents/Frameworks/QtWidgets.framework/QtWidgets "Core Gui"
-change "$APP"/Contents/Frameworks/QtMacExtras.framework/QtMacExtras "Core Gui Widgets"
-change "$APP"/Contents/Frameworks/QtPrintSupport.framework/QtPrintSupport "Core Gui Widgets"
-change "$APP"/Contents/Plugins/platforms/libqcocoa.dylib "Core Gui Widgets PrintSupport"
-
 cat >"$APP"/Contents/MacOS/qt.conf <<EOF
 [Paths]
 Plugins = Plugins
 EOF
 
-zip -q -r9 "$TARGET".zip "$TARGET"
+7za a -mx=9 -r -tzip "$TARGET".zip "$TARGET"
+
+gpg --detach-sign "$TARGET".zip.sig "$TARGET".zip

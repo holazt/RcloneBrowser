5256715629583c82e2d63284cd96929662168ea2
kapitainsky <dariuszb@me.com>
2019-10-19 08:24:26 +0100

    windows release script update

--- scripts/release_windows.cmd
+++ scripts/release_windows.cmd
@@ -1,15 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
 
-if "%1" == "" (
-  echo No architecture x86 or x64 specified in cmdline!
-  goto :eof
-)
-
-set ARCH=%1
-call "%VS120COMNTOOLS%..\..\VC\vcvarsall.bat" %ARCH%
-
-set QT=C:\Qt\5.8.0-vs2013-%ARCH%
+set QT=C:\Qt\5.13.1\msvc2017_64\
 set PATH=%QT%\bin;%PATH%
 
 set ROOT="%~dp0.."
@@ -25,20 +17,17 @@ if "%ERRORLEVEL%" equ "0" (
   set VERSION=%VERSION%-!COMMIT!
 )
 
-if "%ARCH%" == "x86" (
-  set TARGET="%~dp0rclone-browser-%VERSION%-win32"
-  set CMAKEGEN="Visual Studio 12"
-) else (
-  set TARGET="%~dp0rclone-browser-%VERSION%-win64"
-  set CMAKEGEN="Visual Studio 12 Win64"
-)
+set TARGET="%~dp0\..\release\rclone-browser-%VERSION%-win64"
 
 pushd "%ROOT%"
+
+if not exist release mkdir release
+
 if exist build rd /s /q build
 mkdir build
 cd build
 
-cmake .. -G %CMAKEGEN% -DCMAKE_CONFIGURATION_TYPES="Release"
+cmake -G "Visual Studio 16 2019" -A x64 -DCMAKE_CONFIGURATION_TYPES="Release" -DCMAKE_PREFIX_PATH=%QT% ..
 cmake --build . --config Release
 popd
 
@@ -52,14 +41,10 @@ copy "%BUILD%\RcloneBrowser.exe" "%TARGET%"
 windeployqt.exe --no-translations --no-angle --no-compiler-runtime --no-svg "%TARGET%\RcloneBrowser.exe"
 rd /s /q "%TARGET%\imageformats"
 
-copy "%VS120COMNTOOLS%..\..\VC\redist\%ARCH%\Microsoft.VC120.CRT\msvcp120.dll" "%TARGET%"
-copy "%VS120COMNTOOLS%..\..\VC\redist\%ARCH%\Microsoft.VC120.CRT\msvcr120.dll" "%TARGET%"
-
 (
 echo [Paths]
 echo Prefix = .
 echo LibraryExecutables = .
 echo Plugins = .
 )>"%TARGET%\qt.conf"
-7za.exe a -mx=9 -r -tzip "%TARGET%.zip" "%TARGET%"
-rd /s /q "%TARGET%"
+"c:\Program Files\7-Zip\7z.exe" a -mx=9 -r -tzip "%TARGET%.zip" "%TARGET%"

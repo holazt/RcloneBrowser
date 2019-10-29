333f799b6b231bbf84631cbce96ad621f58d1c7e
kapitainsky <dariuszb@me.com>
2019-10-29 10:57:37 +0000

    enforce one instance of Rclone Browser per user
    The lock file is created in a temporary directory that is specific to a logged in user. This means that each user on the system will be able to run one instance of the app for it’s own.
    
    There is no danger when the app crashes, because the lock will be destroyed with the app instance.

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,21 @@ int main(int argc, char *argv[]) {
   app.setOrganizationName("rclone-browser");
   app.setWindowIcon(QIcon(":/icons/icon.png"));
 
+  // enforce one instance of Rclone Browser per user
+  QString tmpDir = QDir::tempPath();
+  QLockFile lockFile(tmpDir + "/RcloneBrowser_4q6RRpbJA.lock");
+
+  if (!lockFile.tryLock(100)) {
+    // if already running display warning and quit
+    QMessageBox msgBox;
+    msgBox.setIcon(QMessageBox::Warning);
+    msgBox.setText("Rclone Browser is already running."
+                   "\r\n\nOnly one instance is allowed.");
+    msgBox.exec();
+    return static_cast<int>(
+        0x80004004); // exit immediately if another instance is running
+  }
+
   MainWindow w;
   w.show();
 

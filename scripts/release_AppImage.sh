cc7810418829c717fe37f7d6c506f7fb518a6fbc
kapitainsky <dariuszb@me.com>
2019-11-07 08:25:34 +0000

    release_AppImage.sh - added raspberry pi support

--- scripts/release_AppImage.sh
+++ scripts/release_AppImage.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # already set in env
-# export PATH="/home/kapitainsky/Qt/5.13.2/gcc_64/bin/:$PATH"
-# export CPPFLAGS="-I/home/kapitainsky/Qt/5.13.2/gcc_64/include/"
-# export LDFLAGS="-L/home/kapitainsky/Qt/5.13.2/gcc_64/lib/"
-# export LD_LIBRARY_PATH="/home/kapitainsky/Qt/5.13.2/gcc_64/lib/:$LD_LIBRARY_PATH"
+# export PATH="/opt/Qt/5.13.2/bin/:$PATH"
+# export CPPFLAGS="-I/opt/Qt/5.13.2/bin/include/"
+# export LDFLAGS="-L/opt/Qt/5.13.2/bin/lib/"
+# export LD_LIBRARY_PATH="/opt/Qt/5.13.2/bin/lib/:$LD_LIBRARY_PATH"
 
+QTDIR=/opt/Qt/5.13.2
+
+# for x86_64 Linux
 # Qt 5.13.2 uses openssl 1.1 and on Ubuntu 16.04 LTS 1.0 is used so
 # we build openssl 1.1.1d source using following setup:
 # ./config --prefix=/opt/openssl-1.1.1/ && make --jobs=`nproc --all` && sudo make install
@@ -52,28 +55,44 @@ fi
 # build and install to temporary AppDir folder
 cd "$BUILD"
 cmake .. -DCMAKE_INSTALL_PREFIX=/usr
+
+if [ `arch` = "armv7l" ]
+# more processes need swap
+make -j 2
+else
 make --jobs=`nproc --all`
+fi
+
 make install DESTDIR=$TEMP_BASE/$TARGET/AppDir
 
 # prepare AppImage
 cd "$TEMP_BASE/$TARGET"
 
-# https://github.com/linuxdeploy/linuxdeploy
-# https://github.com/linuxdeploy/linuxdeploy-plugin-qt
-linuxdeploy --appdir AppDir
-linuxdeploy-plugin-qt  --appdir AppDir
-
-# we add openssl 1.1.1 libs
-cp /opt/openssl-1.1.1/lib/libssl.so.1.1 ./AppDir/usr/lib/
-cp /opt/openssl-1.1.1/lib/libcrypto.so.1.1 ./AppDir/usr/lib/
-
 # metainfo file
 mkdir $TEMP_BASE/$TARGET/AppDir/usr/share/metainfo
 cp $ROOT/scripts/rclone-browser.appdata.xml $TEMP_BASE/$TARGET/AppDir/usr/share/metainfo/
 
-# https://github.com/linuxdeploy/linuxdeploy-plugin-appimage
-# now can create AppImage from AppDir
-linuxdeploy-plugin-appimage --appdir=AppDir
+
+if [ `arch` = "armv7l" ]
+then
+  # armv7l architecture
+  # For RPi we use https://github.com/probonopd/linuxdeployqt as linuxdeploy-plugin-qt had issues
+  linuxdeployqt AppDir/usr/share/applications/rclone-browser.desktop -qmake=$QTDIR/bin/qmake -no-translations -appimage
+else
+  # x86_64 architecture
+  # https://github.com/linuxdeploy/linuxdeploy
+  # https://github.com/linuxdeploy/linuxdeploy-plugin-qt
+  linuxdeploy --appdir AppDir
+  linuxdeploy-plugin-qt  --appdir AppDir
+
+  # we add openssl 1.1.1 libs to enable ssl for some old Linux distros
+  cp /opt/openssl-1.1.1/lib/libssl.so.1.1 ./AppDir/usr/lib/
+  cp /opt/openssl-1.1.1/lib/libcrypto.so.1.1 ./AppDir/usr/lib/
+
+  # https://github.com/linuxdeploy/linuxdeploy-plugin-appimage
+  # now can create AppImage from AppDir
+  linuxdeploy-plugin-appimage --appdir=AppDir
+fi
 
 rename 's/Rclone_Browser/rclone-browser/' Rclone_Browser*
 
@@ -82,4 +101,3 @@ cp rclone-browser* $ROOT/release/
 # clean AppImage temporary folder
 cd ..
 rm -rf $TARGET
-

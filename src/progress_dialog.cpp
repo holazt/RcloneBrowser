9cd466ef1a01503717bd5d8b37db99dcd624f724
kapitainsky <dariuszb@me.com>
2020-02-29 20:27:04 +0000

    progress dialogs refined

--- src/progress_dialog.cpp
+++ src/progress_dialog.cpp
@@ -1,4 +1,5 @@
 #include "progress_dialog.h"
+#include "utils.h"
 
 ProgressDialog::ProgressDialog(const QString &title, const QString &operation,
                                const QString &message, QProcess *process,
@@ -17,6 +18,47 @@ ProgressDialog::ProgressDialog(const QString &title, const QString &operation,
   ui.labelInfo->setText(message);
 
   ui.output->setFont(QFontDatabase::systemFont(QFontDatabase::FixedFont));
+
+  // apply font size preferences
+  auto settings = GetSettings();
+
+  int fontsize = 0;
+
+  if (settings->value("Settings/fontSize").toString() == "0") {
+    fontsize = 0;
+  }
+  if (settings->value("Settings/fontSize").toString() == "1") {
+    fontsize = 1;
+  }
+  if (settings->value("Settings/fontSize").toString() == "2") {
+    fontsize = 2;
+  }
+  if (settings->value("Settings/fontSize").toString() == "3") {
+    fontsize = 3;
+  }
+  if (settings->value("Settings/fontSize").toString() == "4") {
+    fontsize = 4;
+  }
+  if (settings->value("Settings/fontSize").toString() == "5") {
+    fontsize = 5;
+  }
+  if (settings->value("Settings/fontSize").toString() == "6") {
+    fontsize = 6;
+  }
+  if (settings->value("Settings/fontSize").toString() == "7") {
+    fontsize = 7;
+  }
+  if (settings->value("Settings/fontSize").toString() == "8") {
+    fontsize = 8;
+  }
+  if (settings->value("Settings/fontSize").toString() == "9") {
+    fontsize = 9;
+  }
+
+  QFont font = ui.output->font();
+  QFontMetrics fm(font);
+  font.setPointSize(font.pointSize() + fontsize);
+  ui.output->setFont(font);
 
   ui.output->setVisible(false);
 
@@ -68,9 +110,8 @@ ProgressDialog::ProgressDialog(const QString &title, const QString &operation,
     }
     ui.output->appendPlainText(output);
 
-    ui.labelOperation->setStyleSheet(
-        "QLabel { color: black; font-weight: bold; }");
-    ui.labelOperation->setText("DONE!  ");
+    ui.labelOperation->setVisible(false);
+    ;
 
     emit outputAvailable(output);
   });

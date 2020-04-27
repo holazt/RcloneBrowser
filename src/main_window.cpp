dcb9283c6320693832394381530c5ebfbe9cc603
kapitainsky <dariuszb@me.com>
2020-04-27 16:56:23 +0100

    refined fusion dark mode

--- src/main_window.cpp
+++ src/main_window.cpp
@@ -62,6 +62,8 @@ MainWindow::MainWindow() {
     darkPalette.setColor(QPalette::Text, Qt::white);
     darkPalette.setColor(QPalette::Disabled, QPalette::Text,
                          QColor(127, 127, 127));
+    darkPalette.setColor(QPalette::Disabled, QPalette::Light,
+                         QColor(35, 35, 35));
     darkPalette.setColor(QPalette::Dark, QColor(35, 35, 35));
     darkPalette.setColor(QPalette::Shadow, QColor(20, 20, 20));
     darkPalette.setColor(QPalette::Button, QColor(53, 53, 53));
@@ -97,23 +99,35 @@ MainWindow::MainWindow() {
       QPalette darkPalette;
       darkPalette.setColor(QPalette::Window, QColor(53, 53, 53));
       darkPalette.setColor(QPalette::WindowText, Qt::white);
-      darkPalette.setColor(QPalette::Base, QColor(25, 25, 25));
-      darkPalette.setColor(QPalette::AlternateBase, QColor(53, 53, 53));
+      darkPalette.setColor(QPalette::Disabled, QPalette::WindowText,
+                           QColor(127, 127, 127));
+      darkPalette.setColor(QPalette::Base, QColor(42, 42, 42));
+      darkPalette.setColor(QPalette::AlternateBase, QColor(66, 66, 66));
       darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
       darkPalette.setColor(QPalette::ToolTipText, Qt::white);
       darkPalette.setColor(QPalette::Text, Qt::white);
+      darkPalette.setColor(QPalette::Disabled, QPalette::Text,
+                           QColor(127, 127, 127));
+      darkPalette.setColor(QPalette::Disabled, QPalette::Light,
+                           QColor(35, 35, 35));
+      darkPalette.setColor(QPalette::Dark, QColor(35, 35, 35));
+      darkPalette.setColor(QPalette::Shadow, QColor(20, 20, 20));
       darkPalette.setColor(QPalette::Button, QColor(53, 53, 53));
       darkPalette.setColor(QPalette::ButtonText, Qt::white);
+      darkPalette.setColor(QPalette::Disabled, QPalette::ButtonText,
+                           QColor(127, 127, 127));
       darkPalette.setColor(QPalette::BrightText, Qt::red);
       darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
-
       darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
-      darkPalette.setColor(QPalette::HighlightedText, Qt::black);
-
+      darkPalette.setColor(QPalette::Disabled, QPalette::Highlight,
+                           QColor(80, 80, 80));
+      darkPalette.setColor(QPalette::HighlightedText, Qt::white);
+      darkPalette.setColor(QPalette::Disabled, QPalette::HighlightedText,
+                           QColor(127, 127, 127));
       qApp->setPalette(darkPalette);
-
-      qApp->setStyleSheet("QToolTip { color: #ffffff; background-color: "
-                          "#2a82da; border: 1px solid white; }");
+      qApp->setStyleSheet(
+          "QToolTip { color: #ffffff; background-color: #2a82da; "
+          "border: 1px solid white;}");
     }
   }
 #endif
